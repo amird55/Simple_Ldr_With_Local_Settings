d62ba0bf2caf5b53ea22840139f10d4095fca470
amird55 <amir.dgn@outlook.com>
2024-06-14 10:36:06 +0300

    דף מלא עם כפתורים וניתוח מידע שמתקבל מהם

--- Simple_Ldr_With_Local_Settings.ino
+++ Simple_Ldr_With_Local_Settings.ino
@@ -1,6 +1,9 @@
 #define LdrPin A0
 #define LedPin D2
 
+int LdrVal;
+int LedVal;
+int MapDir=1;
 void setup() {
   Serial.begin(9600);
   pinMode(LedPin,OUTPUT);
@@ -8,9 +11,13 @@ void setup() {
 }
 
 void loop() {
-  int LdrVal = analogRead(LdrPin);
+  LdrVal = analogRead(LdrPin);
   Serial.println(LdrVal);
-  int LedVal=map(LdrVal,0,1023,255,0);
+  if(MapDir == 1){
+    LedVal=map(LdrVal,0,1023,0,255);
+  } else {
+    LedVal=map(LdrVal,0,1023,255,0);
+  }
   analogWrite(LedPin,LedVal);
   wifi_loop();
 }
--- k10_wifi_local.ino
+++ k10_wifi_local.ino
@@ -37,8 +37,58 @@ void handleNotFound(){
   server.send(404, "text/plain", message);
 }
 void handleRoot(){
+  if (server.hasArg("btn_pressed")) {
+    //MapDir = (server.arg("btn_pressed")=="1") ? 1 : 2;
+    MapDir = server.arg("btn_pressed").toInt();
+  }
+  
+
   char html[2000]="";
 	strcat(html, "<html>");
+	strcat(html, "<head>");
+	strcat(html, "<meta charset=\"UTF-8\">");
+	strcat(html, "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
+	strcat(html, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+	strcat(html, "<META HTTP-EQUIV=\"refresh\" CONTENT=\"1\">");
+	strcat(html, "<title>Big_Number</title>");
+  //beginig of my css
+  strcat(html, "<style>");
+  // the css body 
+  strcat(html, "body{");
+  strcat(html, " justify-content: center;");
+  strcat(html, " align-items: center;");
+  strcat(html, "  background-color: yellow;");
+  strcat(html, "  };");
+  //the css for big number 
+  strcat(html, "  .BigNumber{");
+  strcat(html, "  font-size: 100px;;");
+  strcat(html, "  width: 30px;");
+  strcat(html, "  align-items: center;");
+  strcat(html, "  justify-content: center;;");
+  strcat(html, "  };");
+  // THE END OF MY CSS 
+  strcat(html, "</style>");
+  //the end of the head
+  strcat(html, "</head>");
+  // beginig of the body 
+  strcat(html, "<body>");
+  strcat(html, "<h2>LDR value</h2>");
+  // LdrVal is from main tab
+  char strTmp[5];
+  itoa(LdrVal,strTmp,10);
+  strcat(html, "<div class=\"BigNumber\">");
+  strcat(html, strTmp);
+  strcat(html, "</div>");
+
+  strcat(html, "<form action='' method='get'>");
+  strcat(html, "<button name='btn_pressed' value='1'>אור סינכרוני</button>");
+  strcat(html, "<button name='btn_pressed' value='2'>אור הפוך</button>");
+  strcat(html, "</form>");
+
+  // the end of the body
+  strcat(html, "</body>");
+  // the end of my html documnet
+ strcat(html, "</html>");
 
   server.send(200, "text/html", html);
 }
